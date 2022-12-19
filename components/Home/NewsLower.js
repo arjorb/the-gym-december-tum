2fb723e2dd72b0438a9f154baff90833b3fb6800
John U <arjorb@gmail.com>
2022-12-19 12:16:03 +0200

    done fixing a margin on news

--- components/Home/NewsLower.js
+++ components/Home/NewsLower.js
@@ -1,32 +1,35 @@
 import Container from '../ui/Container';
 import NewsCard from '../shared/NewsCard';
-import ranking from '../../public/ranking.jpg';
-import research from '../../public/research.jpg';
+import leibinz from '../../public/leibinz.jpg';
+import sustain from '../../public/sustain.jpg';
 
 const NewsLower = () => {
   return (
     <Container>
-      <div className='my-10 grid grid-cols-4 gap-5'>
-        <div className=''></div>
-        <NewsCard
-          className='col-start-3'
-          image={research}
-          span='Research'
-          category='Search for dark matter'
-          title='Antihelium nuclei as messengers from the depths of the galaxy'
-          content='How are galaxies born, and what holds them together? Astronomers assume that dark matter plays an essential role. However,...'
-          date='12/13/2022'
-          time='Reading time 2 min'
-        />
-        <NewsCard
-          image={research}
-          span='Research'
-          category='Search for dark matter'
-          title='Antihelium nuclei as messengers from the depths of the galaxy'
-          content='How are galaxies born, and what holds them together? Astronomers assume that dark matter plays an essential role. However,...'
-          date='12/13/2022'
-          time='Reading time 2 min'
-        />
+      <div className='my-10 flex gap-6 justify-end mr-48'>
+        <div className='w-[280px]'>
+          <NewsCard
+            className='col-start-3'
+            image={leibinz}
+            span='Research'
+            category='Germany´s top research award'
+            title='Leibniz Prize for Biomathematician Fabian Theis'
+            content='Mathematician, physicist and information scientist Prof. Fabian Theis has won the 2023 Gottfried Wilhelm Leibniz Prize. The...'
+            date='12/08/2022'
+            time='Reading time 2 min'
+          />
+        </div>
+        <div className='w-[280px]'>
+          <NewsCard
+            image={sustain}
+            span='Sustainability'
+            category='Awards for sustainability projects'
+            title='New partnership with the Nobel Sustainability Trust'
+            content='The Nobel Sustainability Trust and our university will jointly recognize international research and initiatives for...'
+            date='12/14/2022'
+            time='Reading time 2 min'
+          />
+        </div>
       </div>
     </Container>
   );
